083c5ee07a557ac1c5765bfc8a9f4f574393678b
CouetteChauffante <s.jeames@rubika-edu.com>
2025-03-20 13:08:08 +0100

    Fix de l'ordre des scenes + mort trigger automatiquement la prochaine scene

--- Assets/Scripts/Managers/LifeManager.cs
+++ Assets/Scripts/Managers/LifeManager.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class LifeManager : MonoBehaviour
 {
     public TextMeshProUGUI lifeText; // Reference to the TextMeshProUGUI component
     private int life = 90;
+    private int lifeTotal = 100;
 
     void Start()
     {
@@ -29,6 +31,13 @@ public class LifeManager : MonoBehaviour
     void UpdateLifeText()
     {
         lifeText.text = "Vie: " + life.ToString("00");
+        lifeTotal -= 10;
+        Debug.Log("Vie = "+lifeTotal);
+        if (lifeTotal == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
     }
 
     public void RemoveLife(int amount)
